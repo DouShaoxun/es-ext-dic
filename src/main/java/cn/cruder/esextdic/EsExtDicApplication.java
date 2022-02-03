47f5103a9eb78df56b21ef219976d5151f0deb38
DouShaoxun <dousx_cn@163.com>
2022-02-03 14:13:05 +0800

    IK Analyzer 远程扩展字典

---
+++ src/main/java/cn/cruder/esextdic/EsExtDicApplication.java
@@ -0,0 +1,13 @@
+package cn.cruder.esextdic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsExtDicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsExtDicApplication.class, args);
+    }
+
+}
